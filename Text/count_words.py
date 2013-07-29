f188831e991ad9072c4c3a28a9890e2289637157
ronnyma <ronnyma@transfinite.no>
2013-07-29 15:12:00 +0200

    Removed unused code

--- Text/count_words.py
+++ Text/count_words.py
@@ -2,7 +2,7 @@
 
 """
 Count Words in a String - Counts the number of individual
-words in a string and display the top 5/10 most used words.
+words in a string and display the top n most used words.
 """
 
 import re
@@ -17,16 +17,14 @@ class WordCount:
     def count_words(self, most_used):
         tokenized = re.split(r'\W+', (open(self.infile).read()).strip())  #Split the text by non-words
         
-        #result = dict([(i, tokenized.count(i)) for i in set(tokenized)])  #Put the tokes into a dict to count unique
 
         sresult = sorted((dict([(i, tokenized.count(i)) for i in set(tokenized)])).items(), key=lambda x: -x[1])  #Sort reverse
 
-        print sresult
 
 
         print "The text contains %d words where %d are unique words" % (len(tokenized), len(sresult) )
 
-        print "Top ten words by frequency:"
+        print "Top words by frequency:"
         print [el[0] for el in sresult][:most_used]
 
 
